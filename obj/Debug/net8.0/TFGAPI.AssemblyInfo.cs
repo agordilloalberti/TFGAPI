94f57a95ccb1883c3ea8ef8f4cbee9217b4f66aa
Alvarogogal <agorsua0610@g.educaand.es>
2025-03-13 19:04:02 +0100

    First Commit

--- obj/Debug/net8.0/TFGAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/TFGAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TFGAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+645a715eb6559291f8131b24659da5df6c9dc180")]
 [assembly: System.Reflection.AssemblyProductAttribute("TFGAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TFGAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
